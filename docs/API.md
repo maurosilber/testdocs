783ea122dfeb00e1aa4be3c43961415690c78b3c
Mauro Silberberg <maurosilber@gmail.com>
2024-12-04 19:48:33 -0300

    New point

--- docs/API.md
+++ docs/API.md
@@ -1,1 +1,2 @@
 ::: testdocs.Point
+::: testdocs.Point3
--- src/testdocs/__init__.py
+++ src/testdocs/__init__.py
@@ -5,3 +5,10 @@ from dataclasses import dataclass
 class Point:
     x: int
     y: int
+
+
+@dataclass
+class Point3:
+    x: int
+    y: int
+    z: int
